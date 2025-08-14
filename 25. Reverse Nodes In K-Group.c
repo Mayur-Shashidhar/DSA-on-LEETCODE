c56101c75966a1348a211838e7edc023412c71f2
Mayur Shashidhar <Mayurmamatha9916@gmail.com>
2025-08-14 16:18:55 +0530

    Create 25. Reverse Nodes In K-Group.c

--- 25. Reverse Nodes In K-Group.c
+++ 25. Reverse Nodes In K-Group.c
@@ -1,3 +1,4 @@
+/*
 QUESTION:
 Given the head of a linked list, reverse the nodes of the list k at a time, and return the modified list.
 k is a positive integer and is less than or equal to the length of the linked list. If the number of nodes is not a multiple of k then left-out nodes, in the end, should remain as it is.
@@ -21,10 +22,8 @@ Steps:
   end of reversed group -> next group's start
   Move prevGroup to the end of the reversed group.
 5.Stop when fewer than k nodes remain.
+*/
 
-
-
-CODE:
 #include <stdio.h>
 #include <stdlib.h>
 
