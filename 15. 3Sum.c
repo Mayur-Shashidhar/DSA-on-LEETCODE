d8a5f911bc219c4b2b9808feeb30cc8d37caef17
Mayur Shashidhar <Mayurmamatha9916@gmail.com>
2025-08-14 16:13:21 +0530

    Create 15. 3Sum.c

--- 15. 3Sum.c
+++ 15. 3Sum.c
@@ -1,3 +1,4 @@
+/*
 QUESTION:
 Given an integer array nums, return all the triplets [nums[i], nums[j], nums[k]] such that i != j, i != k, and j != k, and nums[i] + nums[j] + nums[k] == 0.
 Notice that the solution set must not contain duplicate triplets.
@@ -16,10 +17,8 @@ We can solve it efficiently with:
     2.If sum is greater than target → move right backward.
     3.Else → record the triplet and skip duplicates.
 6.Continue until all unique triplets are found.
+*/
 
-
-
-CODE:
 #include <stdio.h>
 #include <stdlib.h>
 
