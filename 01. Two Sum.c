ac9dc680354c2885111d3d1d338a1fd3f083276c
Mayur Shashidhar <Mayurmamatha9916@gmail.com>
2025-08-14 14:19:50 +0530

    Update and rename 01. Two Sum to 01. Two Sum.c

--- 01. Two Sum.c
+++ 01. Two Sum.c
@@ -1,4 +1,4 @@
-QUESTION :
+/* QUESTION :
 Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
 You may assume that each input would have exactly one solution, and you may not use the same element twice.
 You can return the answer in any order.
@@ -8,7 +8,7 @@ APPROACH:
 2.For each element, check if there exists another element that sums up to the target.
 3.Return the pair of indices when found.
 
-CODE:
+CODE: */
 #include <stdlib.h> // For malloc()
 
 
