aef03b60fcdd38e5a8b16cfd1d426021b3f235ed
Mayur Shashidhar <Mayurmamatha9916@gmail.com>
2025-08-14 16:22:50 +0530

    Create 87. Scramble String.c

--- 87. Scramble String.c
+++ 87. Scramble String.c
@@ -1,3 +1,4 @@
+/*
 QUESTION:
 We can scramble a string s to get a string t using the following algorithm:
 1.If the length of the string is 1, stop.
@@ -11,10 +12,8 @@ Given two strings s1 and s2 of the same length, return true if s2 is a scrambled
 
 APPROACH:
 Top‑Down DP + Memoization
+*/
 
-
-
-CODE:
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
