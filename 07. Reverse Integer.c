e9858fe8057ba492e25039214a4f8c4465f75220
Mayur Shashidhar <Mayurmamatha9916@gmail.com>
2025-08-14 16:07:33 +0530

    Create 07. Reverse Integer.c

--- 07. Reverse Integer.c
+++ 07. Reverse Integer.c
@@ -1,3 +1,4 @@
+/*
 QUESTION:
 Given a signed 32-bit integer x, return x with its digits reversed. If reversing x causes the value to go outside the signed 32-bit integer range [-231, 231 - 1], then return 0.
 Assume the environment does not allow you to store 64-bit integers (signed or unsigned).
@@ -11,9 +12,8 @@ APPROACH:
         If rev == INT_MAX / 10 and pop > 7 → overflow
         If rev < INT_MIN / 10 → overflow
         If rev == INT_MIN / 10 and pop < -8 → overflow
+*/
 
-
-CODE:
 #include <stdio.h>
 #include <limits.h> // For INT_MAX and INT_MIN
 
